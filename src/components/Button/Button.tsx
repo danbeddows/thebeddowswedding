4743f4b712e7921deb04a4d274a8348a9155218b
Dan Beddows <dan@cloudmix.net>
2022-03-10 22:56:30 +0000

    added Subscribe. updated Button and Input

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,30 +1,68 @@
-import { useEffect, useState } from "react";
+import { faSpinnerThird } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { createRef, useEffect, useRef, useState } from "react";
 import StyledButton from "./Button.styles";
 
 interface ButtonProps {
-  label: string;
   isLoading?: boolean;
+  onClick: () => void;
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, isLoading = false }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  isLoading = false,
+  className = "",
+}) => {
+  const btnRef = createRef<HTMLButtonElement>();
   const [isDisabled, setIsDisabled] = useState(false);
+  const [content, setContent] = useState(children);
+  const [width, setWidth] = useState<"auto" | number | string>("auto");
+  const [prevWidth, setPrevWidth] = useState<"auto" | number | string>("auto");
 
-  const evaluateIsDisabled = () => {
-    if (isLoading === true) {
-      setIsDisabled(true);
-    } else {
-      setIsDisabled(false);
-    }
+  const handleOnClick = () => {
+    onClick();
   };
 
+  // When loading state changes, reevaluate the button content
+  // If showing the spinner, record the btn width so there's no layout shift
   useEffect(() => {
-    evaluateIsDisabled();
+    if (isLoading) {
+      // If btn is rendered, set the width before we change the content
+      if (btnRef.current) {
+        const styles = window.getComputedStyle(btnRef.current);
+
+        if (styles.width !== null) {
+          setPrevWidth(styles.width);
+        } else {
+          setPrevWidth("auto");
+        }
+
+        setWidth(btnRef.current.offsetWidth);
+      }
+
+      // Change the content to the loading spinner
+      setContent(<FontAwesomeIcon icon={faSpinnerThird} spin fixedWidth />);
+
+      setIsDisabled(true);
+    } else {
+      setWidth(prevWidth);
+      setContent(children);
+      setIsDisabled(false);
+    }
   }, [isLoading]);
 
   return (
-    <StyledButton type="button" disabled={isDisabled}>
-      {isLoading && <>loading</>}
-      {!isLoading && label}
+    <StyledButton
+      ref={btnRef}
+      type="button"
+      disabled={isDisabled}
+      onClick={handleOnClick}
+      style={{ width }}
+      className={className}
+    >
+      {content}
     </StyledButton>
   );
 };
