238e61f22ede0dde742894154efa43c48cb1f826
Dan Beddows <dan@cloudmix.net>
2022-11-21 21:15:14 +0000

    Added maps redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  pageExtensions: ["tsx"], // set only to tsx to ignore js files (js is used for styled components)
+  pageExtensions: ["tsx"],
+  async redirects() {
+    return [
+      {
+        source: "/applemaps",
+        destination:
+          "https://maps.apple.com/?address=Holford%20Mill,%20Chester%20Road,%20Plumley,%20Knutsford,%20WA16%200UA,%20England&ll=53.277014,-2.441687&q=Holford%20Mill&_ext=EiYpuJuW/+GiSkAxTd2JXfWXA8A5NnG8WwikSkBBdVUH0DB5A8BQBA%3D%3D",
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: "/googlemaps",
+        destination: "https://goo.gl/maps/qNtTC4xCpFxhiYm68",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
