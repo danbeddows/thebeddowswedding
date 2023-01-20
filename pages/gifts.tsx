b96d530282e57484e80f5bb7402e02aa9db9e345
Dan Beddows <dan@cloudmix.net>
2023-01-20 21:25:38 +0000

    Added bg

--- pages/gifts.tsx
+++ pages/gifts.tsx
@@ -1,11 +1,42 @@
+import { faArrowDown } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PageHeading from "src/components/PageHeading/PageHeading";
-import { GiftPage } from "./gifts.styles";
+import Paragraph from "src/components/Paragraph";
+import {
+  GiftPage,
+  GiftsHero,
+  GiftsHeroArrow,
+} from "../src/styles/pages/gifts.styles";
 
 const Gifts = () => {
   return (
-    <GiftPage>
-      <PageHeading>Wedding Gifts</PageHeading>
-    </GiftPage>
+    <>
+      {" "}
+      <GiftsHeroArrow>
+        <FontAwesomeIcon icon={faArrowDown} />
+      </GiftsHeroArrow>
+      <GiftsHero />
+      <GiftPage>
+        <PageHeading>Wedding Gifts</PageHeading>
+        <Paragraph>
+          We’re incredibly thankful to have everything we need, so we don’t have
+          a gift registry.{" "}
+        </Paragraph>
+        <Paragraph>You joining us to celebrate really is enough.</Paragraph>
+        <Paragraph>
+          But if you’d like to, you can help us create some new memories by
+          contributing to our honeymoon.
+        </Paragraph>
+        <Paragraph>
+          In September 2023, we'll be setting off on an adventure around Hawaii.
+          We'll be going to O'ahu to see Pearl Harbour and hike up Diamond Head,
+          then to Maui to hit the beach and drive the Road to Hana, then Kauai
+          to go to a Luau and explore the garden isle and finally Big Island to
+          hike through the Volcano National Park and do a Kona coffee tour. We
+          can’t wait!
+        </Paragraph>
+      </GiftPage>
+    </>
   );
 };
 
---
+++ src/styles/pages/gifts.styles.ts
@@ -0,0 +1,61 @@
+import { rem } from "polished";
+import styled from "styled-components";
+
+export const GiftsHeroArrow = styled.div`
+  width: 100%;
+  height: 100vh;
+  z-index: 100;
+  position: absolute;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  color: #fff;
+  padding-bottom: 18px;
+  font-size: 32px;
+`;
+
+export const GiftsHero = styled.section`
+  width: 100%;
+  height: 100vh;
+  background: transparent url("/img/pages/gifts/hawaii.jpg") no-repeat;
+  background-position: center bottom;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0 ${rem(30)};
+
+  @media (min-width: ${(props) => props.theme.bp.tablet}) {
+    background: transparent url("/img/pages/gifts/hawaii.jpg") no-repeat;
+    background-position: center center;
+    background-size: cover;
+    align-items: flex-end;
+    justify-content: center;
+  }
+
+  @media (min-width: ${(props) => props.theme.bp.desktopLarge}) {
+    background: transparent url("/img/pages/gifts/hawaii.jpg") no-repeat;
+    background-position: center center;
+    background-size: cover;
+    align-items: flex-end;
+    justify-content: center;
+  }
+`;
+
+export const GiftPage = styled.div`
+  max-width: 1200px;
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  @media (min-width: ${(props) => props.theme.bp.desktop}) {
+    margin: 140px 40px;
+  }
+
+  > * {
+    margin-bottom: ${rem(20)};
+  }
+`;
