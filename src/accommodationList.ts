35e825406b1181911369df4f00031360c1d78824
Dan Beddows <dan@cloudmix.net>
2023-01-24 22:43:42 +0000

    Laburnum and holiday inn added

--- src/accommodationList.ts
+++ src/accommodationList.ts
@@ -24,6 +24,30 @@ const theHintonGuestHouse: HotelProps = {
     "https://www.google.com/maps/place/The+Hinton+Guest+House/@53.3130759,-2.3296918,17z/data=!3m1!4b1!4m8!3m7!1s0x487a53bd3a5e7573:0x536814022affb8fa!5m2!4m1!1i2!8m2!3d53.313075!4d-2.3276141",
 };
 
+const laburnumCottageGuestHouse: HotelProps = {
+  name: "Laburnum Cottage Guest House",
+  link: {
+    url: "https://www.laburnumcottageguesthouse.co.uk/",
+  },
+  image: "/img/pages/accommodation/hotels/laburnum-cottage.jpg",
+  appleMaps:
+    "https://maps.apple.com/?address=Knutsford%20Road,%20Knutsford%20Cheshire,%20WA16%207PU,%20United%20Kingdom&auid=3348566101337433504&ll=53.314042,-2.343662&lsp=9902&q=Laburnum%20Cottage%20Guest%20House&_ext=CjIKBQgEEOEBCgQIBRADCgQIBhALCgQIChAACgQIUhAECgQIVRAACgQIWRABCgUIpAEQARImKchMs3uep0pAMdeywagAzwLAOUYi2dfEqEpAQWciu0c1sALAUAQ%3D",
+  googleMaps:
+    "https://www.google.com/maps/place/Laburnum+Cottage+Guest+House/@53.3136412,-2.3464073,17z/data=!3m1!4b1!4m8!3m7!1s0x487a538ff54b47d1:0x2dd4eb6ab9a03c4e!5m2!4m1!1i2!8m2!3d53.3136381!4d-2.3442188",
+};
+
+const holidayInnExpress: HotelProps = {
+  name: "Holiday Inn Express",
+  link: {
+    url: "https://www.ihg.com/holidayinnexpress/hotels/gb/en/manchester/mchma/hoteldetail",
+  },
+  image: "/img/pages/accommodation/hotels/holiday-inn.jpg",
+  appleMaps:
+    "https://maps.apple.com/?address=Runger%20Lane,%20Manchester,%20M90%205DL,%20England&auid=7873188460557799964&ll=53.362579,-2.293940&lsp=9902&q=Holiday%20Inn%20Express%20Manchester%20Airport&_ext=CjIKBQgEEOEBCgQIBRADCgQIBhALCgQIChAACgQIUhADCgQIVRANCgQIWRABCgUIpAEQARImKXth8QLGrUpAMSWyGM98aQLAOfk2F1/srkpAQWOHhYmoSgLAUAQ%3D",
+  googleMaps:
+    "https://www.google.com/maps/place/Holiday+Inn+Express+Manchester+Airport,+an+IHG+Hotel/@53.3626198,-2.2961577,17z/data=!3m1!4b1!4m8!3m7!1s0x487a52ce0339bb51:0x7c15e35040966f60!5m2!4m1!1i2!8m2!3d53.3626166!4d-2.293969",
+};
+
 const cottonsHotelAndSpa: HotelProps = {
   name: "Cotton's Hotel & Spa",
   link: {
@@ -36,6 +60,12 @@ const cottonsHotelAndSpa: HotelProps = {
     "https://www.google.com/maps/place/Cottons+Hotel+%26+Spa/@53.4740486,-2.5647201,10z/data=!3m1!5s0x487a5459997592e9:0xfa17aec95bf97d83!4m8!3m7!1s0x487a546a1c45944d:0xfe588544a4e35f7f!5m2!4m1!1i2!8m2!3d53.3194444!4d-2.3927778?shorturl=1",
 };
 
-const accomList = [theRoebuckInn, theHintonGuestHouse, cottonsHotelAndSpa];
+const accomList = [
+  theRoebuckInn,
+  theHintonGuestHouse,
+  laburnumCottageGuestHouse,
+  holidayInnExpress,
+  cottonsHotelAndSpa,
+];
 
 export default accomList;
